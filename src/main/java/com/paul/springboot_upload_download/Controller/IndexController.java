35d68a78b22165674484d7d312b0901e9cd4dd2c
xiaod <mogu9630@163.com>
2019-07-21 12:37:37 +0800

    springboot文件上传下载_19/7/21

---
+++ src/main/java/com/paul/springboot_upload_download/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.paul.springboot_upload_download.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
---
+++ src/main/java/com/paul/springboot_upload_download/SpringbootUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.paul.springboot_upload_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUploadDownloadApplication.class, args);
+    }
+
+}
